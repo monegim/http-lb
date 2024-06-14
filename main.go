4e0974631a70f79c79807d190a6fea62883fda2f
Mostafa Negim <mostafa.negim@gmail.com>
2024-06-14 22:01:52 +0330

    Add: healthcheck

--- main.go
+++ main.go
@@ -2,12 +2,13 @@ package main
 
 import (
 	"http-lb/internal"
-	"io"
 	"log"
 	"net/http"
 	"net/url"
 	"os"
 	"slices"
+	"sync"
+	"sync/atomic"
 )
 
 var (
@@ -28,54 +29,99 @@ func main() {
 	if port == "" {
 		port = "8080"
 	}
-	http.HandleFunc("/", RootHandler)
-	log.Fatal(http.ListenAndServe(":8080", nil))
+	http.HandleFunc("/", lb)
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
+}
+
+type ServerPool struct {
+	backends []*Backend
+	current  uint64
+}
+
+// AddBackend to the server pool
+func (s *ServerPool) AddBackend(backend *Backend) {
+	s.backends = append(s.backends, backend)
+}
+
+func (s *ServerPool) NextIndex() int {
+	return int(atomic.AddUint64(&s.current, uint64(1)) % uint64(len(s.backends)))
+}
+
+func (s *ServerPool) MarkBackendStatus(backendUrl *url.URL, alive bool) {
+	for _, b := range s.backends {
+		if b.Address == backendUrl.String() {
+			b.SetAlive(alive)
+			break
+		}
+	}
+}
+
+func (s *ServerPool) HealthCheckBackends() {
+	for _, b := range s.backends {
+		status := "up"
+		alive := b.isBackendAlive()
+		b.SetAlive(alive)
+		if !alive {
+			status = "down"
+		}
+		log.Printf("%s [%s]\n", b.Address, status)
+	}
+}
+
+func (b *Backend) SetAlive(alive bool) {
+	b.mux.RLock()
+	b.Alive = alive
+	defer b.mux.RUnlock()
+	return
 }
 
 type Backend struct {
 	Address     string
-	HealthCheck HealthChecks
+	mux         *sync.RWMutex
+	HealthCheck HealthCheck
+	Alive       bool
 }
-type HealthChecks struct {
+type HealthCheck struct {
 	Path               string
 	Timeout            int
 	ExpectedStatusCode []int
-	Ready              bool
 }
 
-func RootHandler(w http.ResponseWriter, r *http.Request) {
-	backend := backends[requestCounter%len(backends)]
-	requestCounter++
-	u, err := url.Parse(backend.Address)
-	if err != nil {
-		w.WriteHeader(http.StatusBadRequest)
-		w.Write([]byte(err.Error()))
-		return
-	}
+func lb(w http.ResponseWriter, r *http.Request) {
 
-	r.RequestURI = ""
-	r.URL.Host = u.Host
-	r.URL.Scheme = u.Scheme
-
-	res, err := client.HttpClient.Do(r)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-	for k, v := range res.Header {
-		w.Header()[k] = v
-	}
-	body := res.Body
-	defer body.Close()
-	b, err := io.ReadAll(body)
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte(err.Error()))
-		return
-	}
-	w.Write(b)
-	LogRequest(r)
+	//requestCounter++
+	//u, err := url.Parse()
+	//if err != nil {
+	//	w.WriteHeader(http.StatusBadRequest)
+	//	w.Write([]byte(err.Error()))
+	//	return
+	//}
+	//
+	//r.RequestURI = ""
+	//r.URL.Host = u.Host
+	//r.URL.Scheme = u.Scheme
+	//
+	//res, err := client.HttpClient.Do(r)
+	//if err != nil {
+	//	w.WriteHeader(http.StatusInternalServerError)
+	//	w.Write([]byte(err.Error()))
+	//	return
+	//}
+	//for k, v := range res.Header {
+	//	w.Header()[k] = v
+	//}
+	//body := res.Body
+	//defer body.Close()
+	//b, err := io.ReadAll(body)
+	//if err != nil {
+	//	w.WriteHeader(http.StatusInternalServerError)
+	//	w.Write([]byte(err.Error()))
+	//	return
+	//}
+	//w.Write(b)
+	//LogRequest(r)
 }
 
 func LogRequest(r *http.Request) {
@@ -86,14 +132,14 @@ func LogRequest(r *http.Request) {
 	log.Println("Accept:", r.Header.Get("Accept"))
 }
 
-func (b *Backend) Ping() (bool, error) {
+func (b *Backend) isBackendAlive() bool {
 	endpoint := b.Address + b.HealthCheck.Path
 	res, err := client.HttpClient.Get(endpoint)
 	if err != nil {
-		return false, err
+		return false
 	}
 	if slices.Contains(b.HealthCheck.ExpectedStatusCode, res.StatusCode) {
-		return true, nil
+		return true
 	}
-	return false, nil
+	return false
 }
